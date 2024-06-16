8332018e27af49ccc01588823985b0491b67d988
SaneyBinFaruk <saneybinfaruk@SaneyBinFaruks-iMac.local>
2024-06-16 21:18:09 +0600

    refactor: cors edit!

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,14 @@ import orders from "./routes/orders";
 import address from "./routes/address";
 
 const app = express();
- 
- 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 // app.use(
 //   cors({
 //     origin: "*",
