6dbe9b93c7a9cc67666647ad09599952a4e380cf
SaneyBinFaruk <saneybinfaruk@SaneyBinFaruks-iMac.local>
2024-06-19 19:15:59 +0600

    refactor: tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { NextFunction, Request, Response } from "express";
-// import "express-async-errors";
 import helmet from "helmet";
 import compression from "compression";
 import users from "./routes/users";
